004c2de25681cba8358c8a3abdb8347906609bbd
rodriguezcarlosa <36528286+candresrod@users.noreply.github.com>
2023-02-02 23:47:25 -0400

    THE GOOD GOOD

--- Assets/Antonio's office/Scripts/Menus/MainMenu.cs
+++ Assets/Antonio's office/Scripts/Menus/MainMenu.cs
@@ -5,7 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    [SerializeField] private GameObject fog;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void PlayGame()
+    {
+        fog.SetActive(true);
+        animator.SetTrigger("Start");
+    }
+
+    public void ChangeScene()
     {
         SceneManager.LoadScene("Tutorial");
     }
--- Assets/Antonio's office/Scripts/Menus/Tutorial.cs
+++ Assets/Antonio's office/Scripts/Menus/Tutorial.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class Tutorial : MonoBehaviour
 {
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void Continue()
+    {
+        animator.SetTrigger("Start");
+    }
+
+    public void StartGame()
     {
         SceneManager.LoadScene("Naomi Scene");
     }
+
 }
